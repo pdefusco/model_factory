07b22abe584ab6b61a75133d5686a294f2c274f5
Paul de Fusco <pauldefusco@cloudera.com>
2020-09-13 05:29:21 +0000

    A/B testing

--- test.py
+++ test.py
@@ -30,8 +30,71 @@ from Experiment import Experiment
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 
-df = pd.read_csv('experiments_summary/experiments_summary.csv') 
+import pickle
+import datetime
+import cdsw
+
+spark = SparkSession\
+    .builder\
+    .appName("TestApp")\
+    .config("spark.hadoop.fs.s3a.s3guard.ddb.region","us-east-1")\
+    .config("spark.yarn.access.hadoopFileSystems","s3a://demo-aws-1/")\
+    .config("spark.hadoop.yarn.resourcemanager.principal",os.getenv("HADOOP_USER_NAME"))\
+    .config("spark.executor.instances", 2)\
+    .config("spark.executor.cores", 2)\
+    .getOrCreate()
+    
+#To do: take in new data rather than old table
+df = spark.sql("select * from default.historical_customer_interactions")
+
+df = df.toPandas()
+
+df = pd.get_dummies(df, columns=["channel", "offer"], drop_first=True)
+df = df.drop(columns=['zip_code'])
+
+y = df['conversion']
+X = df.drop(columns=['conversion'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# Using experiment instance set in CICD.py
+data_dir = "{data}"
+clf = LogisticRegression()
+grid = {'C':[.1,1], 'solver':['lbfgs', 'liblinear'], 'class_weight':['None','balanced']}
+cv = int(3)
 
 
 
-#spark.stop()
+# Simplified Gridsearch... To do: unroll more gs metrics and track them
+gs = GridSearchCV(clf, grid, cv=cv, scoring='accuracy', n_jobs=-1)
+gs.fit(X_train, y_train)
+
+run_time_suffix = datetime.datetime.now()
+run_time_suffix = run_time_suffix.strftime("%d%m%Y%H%M%S")
+
+results = dict()
+results['timestamp'] = run_time_suffix
+results['clf'] = clf.__class__.__name__
+results['best_score'] = gs.best_score_
+#results['best_params'] = [gs.best_params_]
+results['n_splits'] = gs.n_splits_
+results['scorer'] = gs.scorer_
+#results['cv_results'] = [gs.cv_results_]
+#results['grid'] = [grid]
+
+results_df = pd.DataFrame(results, index=[0])
+
+results_df['timestamp'] = results_df['timestamp'].astype(str)
+results_df['clf'] = results_df['clf'].astype(str)
+results_df['best_score'] = results_df['best_score'].astype(float)
+#results_df['best_params'] = results_df['best_params'].astype(str)
+results_df['n_splits'] = results_df['n_splits'].astype(int)
+results_df['scorer'] = results_df['scorer'].astype(str)
+#results_df['cv_results'] = results_df['cv_results'].astype(str)
+#results_df['grid'] = results_df['grid'].astype(str)
+
+experiments_df = spark.createDataFrame(results_df)
+  
+experiments_df.write.insertInto("default.experiment_outcomes",overwrite = False) 
+
+spark.stop()
